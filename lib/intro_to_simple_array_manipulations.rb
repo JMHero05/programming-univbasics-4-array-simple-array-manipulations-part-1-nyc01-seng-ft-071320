c46915513ea873a09c191917e61b888030aa1329
Jake Mills <jake.mills.05@gmail.com>
2020-07-01 17:01:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,13 @@ def using_push(colors_in_the_rainbow, next_color)
 end
 
 def using_unshift(bouroughs_in_nyc, @new_neighborhood)
+
+end
+
+def using_pop(@continents)
+
+end
+
+def pop_with_args(@dog_breeds)
+  
+end

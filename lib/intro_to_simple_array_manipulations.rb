412757ba99bf9236761ce3157c36284eeb1cdbc0
Jake Mills <jake.mills.05@gmail.com>
2020-07-01 17:02:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,22 +2,22 @@ def using_push(colors_in_the_rainbow, next_color)
 
 end
 
-def using_unshift(bouroughs_in_nyc, @new_neighborhood)
+def using_unshift(bouroughs_in_nyc, new_neighborhood)
 
 end
 
-def using_pop(@continents)
+def using_pop(continents)
 
 end
 
-def pop_with_args(@dog_breeds)
+def pop_with_args(dog_breeds)
 
 end
 
-def using_shift(@my_favorite_cities)
+def using_shift(my_favorite_cities)
   
 end
 
-def shift_with_args(@ice_cream_brands)
+def shift_with_args(ice_cream_brands)
   
 end

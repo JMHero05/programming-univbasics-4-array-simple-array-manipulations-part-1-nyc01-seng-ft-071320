e69f552d3f5e473e31d1f3cfccb1af67207f016b
Jake Mills <jake.mills.05@gmail.com>
2020-07-01 17:06:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,8 @@ def using_push(array, string)
   array.push(string)
 end
 
-def using_unshift(bouroughs_in_nyc, new_neighborhood)
-
+def using_unshift(array, string)
+  array.unshift(string)
 end
 
 def using_pop(continents)
